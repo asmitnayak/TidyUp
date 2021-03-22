297a802c686996961531901686183f4e660b9305
kyang236 <48035017+kyang236@users.noreply.github.com>
2021-03-21 19:17:40 -0500

    add background image to leave group page

--- app/src/main/java/com/example/android/tidyup/LeaveGroup.java
+++ app/src/main/java/com/example/android/tidyup/LeaveGroup.java
@@ -1,9 +1,11 @@
 package com.example.android.tidyup;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
@@ -20,22 +22,36 @@ public class LeaveGroup extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_leave_group);
+        createGroupList();
+        buildRecylcerView();
+        myAdapter.setItemClickListener(new RVAdapter.OnItemClickListener() {
+            @Override
+            public void onDeleteClick(int position) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(LeaveGroup.this);
+                builder.setMessage("Are you sure that you want to leave the group?");
+                builder.setPositiveButton( "Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        removeItem(position);
+                    }
+                });
+                builder.setNegativeButton("Cancle", null);
+                builder.create().show();
+            }
+        });
+    }
+    public void createGroupList(){
         myGroupList = new ArrayList<>();
         myGroupList.add(new GroupItems("Apt201",new Button(this)));
         myGroupList.add(new GroupItems("Apt501",new Button(this))) ;
-
+    }
+    public void buildRecylcerView(){
         myRecyclerView = findViewById(R.id.recyclerview);
         myRecyclerView.setHasFixedSize(true);
         myLayoutManager = new LinearLayoutManager(this);
         myAdapter = new RVAdapter(myGroupList);
         myRecyclerView.setLayoutManager(myLayoutManager);
         myRecyclerView.setAdapter(myAdapter);
-        myAdapter.setItemClickListener(new RVAdapter.OnItemClickListener() {
-            @Override
-            public void onDeleteClick(int position) {
-                removeItem(position);
-            }
-        });
     }
     public void removeItem(int position){
         myGroupList.remove(position);

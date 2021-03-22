cf8bc3b192b8a497e91894581f6941905f0060bf
kyang236 <48035017+kyang236@users.noreply.github.com>
2021-03-22 12:43:42 -0500

    rename GroupItem class and activity_groups_item.xml

--- app/src/main/java/com/example/android/tidyup/GroupListObject.java
+++ app/src/main/java/com/example/android/tidyup/GroupListObject.java
@@ -3,11 +3,11 @@ package com.example.android.tidyup;
 import android.app.Activity;
 import android.widget.Button;
 
-public class GroupItems {
+public class GroupListObject {
     private String groupName;
     private Button leaveButton;
 
-    public GroupItems(String groupName, Button leaveButton) {
+    public GroupListObject(String groupName, Button leaveButton) {
         this.groupName = groupName;
         this.leaveButton = leaveButton;
     }

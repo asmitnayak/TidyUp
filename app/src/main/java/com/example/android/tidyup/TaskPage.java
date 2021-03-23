376d11964e2bee12d44cdc36e4ced98fa1d9f794
flygare5 <lflygare@wisc.edu>
2021-03-22 21:59:34 -0500

    adding task page with select option

--- app/src/main/java/com/example/android/tidyup/TaskPage.java
+++ app/src/main/java/com/example/android/tidyup/TaskPage.java
@@ -1,17 +1,45 @@
 package com.example.android.tidyup;
 
-import android.os.Bundle;
-
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
+import android.os.Bundle;
+import android.view.View;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class TaskPage extends AppCompatActivity {
+    public static ArrayList<TaskItem> taskItems = new ArrayList<TaskItem>();
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task_page);
     }
 
-    public static ArrayList<TaskItem> taskItems = new ArrayList<TaskItem>();
+    // when a task is selected and the completeTask button is pressed
+    // the task will be removed from the task page
+    public void completeTask(View view){
+
+    }
+
+
 
 }

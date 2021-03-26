f8a2d264cd9c300a4b34c0d0bc3847fddf98d433
monica22schmidt <monica22schmidt@gmail.com>
2021-03-25 22:43:10 -0500

    Database updates in GroupSettingsAdapter and GroupSettings

--- app/src/main/java/com/example/android/tidyup/GroupSettings.java
+++ app/src/main/java/com/example/android/tidyup/GroupSettings.java
@@ -1,13 +1,19 @@
 package com.example.android.tidyup;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 import android.widget.RadioButton;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentSnapshot;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
@@ -20,11 +26,11 @@ import java.util.Arrays;
 public class GroupSettings extends AppCompatActivity {
     private FirebaseAuth fAuth = FirebaseAuth.getInstance();
     private FirebaseFirestore fFirestore = FirebaseFirestore.getInstance();
-    //private DocumentReference docRef = fFirestore.collection("Groups").document(fAuth.getUid());
+    private DocumentReference docRef = fFirestore.collection("Users").document(fAuth.getUid());
     boolean randomSetting;
     GroupSettingsAdapter customAdp;
     ListView membersList;
-    ArrayList<String> listOfMembers = new ArrayList<String>();
+    ArrayList<String> members = new ArrayList<String>();
     String groupID;
 
     @Override
@@ -32,15 +38,28 @@ public class GroupSettings extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_group_settings);
         membersList = (ListView) findViewById(R.id.membersList);
-//        if(fAuth.getCurrentUser() != null){
-//            //String groupID = fAuth.getCurrentUser().get("Group ID");
-//        }
-//        ArrayList<String> members = GroupManagement.getGroupMemberList(groupID);
-        listOfMembers.add("Max");
-        listOfMembers.add("Rachel");
-        listOfMembers.add("Mark");
+        if(fAuth.getCurrentUser() != null){
+            groupID = "";
+            docRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                if(documentSnapshot.exists()){
+                    groupID = documentSnapshot.getString("GroupID");
+                }else {
+                    return;
+                }
 
-        customAdp = new GroupSettingsAdapter(getApplicationContext(), listOfMembers);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.w("GroupSettings", "Error reading document", e);
+            }
+        });
+        }
+        members = GroupManagement.getGroupMemberList(groupID);
+
+        customAdp = new GroupSettingsAdapter(getApplicationContext(), members);
         membersList.setAdapter(customAdp);
         //randomSetting = get from database
         //members = get from database based off of Users id?
--- app/src/main/java/com/example/android/tidyup/GroupSettingsAdapter.java
+++ app/src/main/java/com/example/android/tidyup/GroupSettingsAdapter.java
@@ -2,6 +2,7 @@ package com.example.android.tidyup;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +11,15 @@ import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -17,6 +27,9 @@ public class GroupSettingsAdapter extends BaseAdapter {
     Context context;
     LayoutInflater inflter;
     ArrayList<String> members;
+    private FirebaseAuth fAuth = FirebaseAuth.getInstance();
+    private FirebaseFirestore fFirestore = FirebaseFirestore.getInstance();
+    private DocumentReference docRef;
 
     public GroupSettingsAdapter(Context applicationContext, ArrayList<String> members) {
         this.context = context;
@@ -44,7 +57,27 @@ public class GroupSettingsAdapter extends BaseAdapter {
     public View getView(int i, View view, ViewGroup viewGroup) {
         view = inflter.inflate(R.layout.member_list_view_layout, null);
         TextView memberName = (TextView) view.findViewById(R.id.memberName);
-        memberName.setText(members.get(i)); //probably get users name from group
+        docRef = fFirestore.collection("Users").document(members.get(i));
+        if(members.get(i) != null){
+            docRef = fFirestore.collection("Users").document(members.get(i));
+            docRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                @Override
+                public void onSuccess(DocumentSnapshot documentSnapshot) {
+                    if(documentSnapshot.exists()){
+                        String name = documentSnapshot.getString("Name");
+                        memberName.setText(name); //probably get users name from group
+                    }else {
+                        return;
+                    }
+
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Log.w("GroupSettings", "Error reading document", e);
+                }
+            });
+        }
         Button delete = (Button) view.findViewById(R.id.deleteButton);
         delete.setOnClickListener(new View.OnClickListener() {
 

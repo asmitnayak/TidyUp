c05bd8fda05a055c0ecd679beab2afce2d7d89b4
monica22schmidt <monica22schmidt@gmail.com>
2021-04-07 00:56:01 -0500

    Added reward and penatly page and started some RewardManagement functionality

--- app/src/main/java/com/example/android/tidyup/RewardsManagement.java
+++ app/src/main/java/com/example/android/tidyup/RewardsManagement.java
@@ -2,10 +2,34 @@ package com.example.android.tidyup;
 
 import android.os.AsyncTask;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.List;
+import java.util.Map;
+
 public class RewardsManagement extends AsyncTask<Void, Void, Void> {
-    private static FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private static final FirebaseAuth fAuth = FirebaseAuth.getInstance();
+    private static final FirebaseFirestore fFirestore = FirebaseFirestore.getInstance();
+    //private static final DocumentReference docRef = fFirestore.collection("Rewards").document(fAuth.getUid());
+    private static Map<String, List<String>> grDB;
+    public static int addReward(String userID, String groupID, String rewardDescription, String rewardName, int rewardVal){
+        if(userID == null || groupID == null || rewardDescription == null || rewardName == null || rewardVal <= 0){
+            return -1; // Missing information
+        }
+        return -1;
+    }
+    public static String getGroupRewards(String groupID){
+        if(grDB == null)
+            return null;
+        for (Map.Entry<String, List<String>> entry : grDB.entrySet()) {
+            if (entry.getValue().contains(groupID)) {
+                return entry.getKey();
+            }
+        }
+        return null;
+    }
     @Override
     protected Void doInBackground(Void... voids) {
         return null;

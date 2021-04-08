d54f0efede644ec3ad016e87f2d9ccaaef87c8a4
monica22schmidt <monica22schmidt@gmail.com>
2021-04-07 23:58:52 -0500

    Added read functionality to rewards management

--- app/src/main/java/com/example/android/tidyup/RewardAndPenatly.java
+++ app/src/main/java/com/example/android/tidyup/RewardAndPenatly.java
@@ -2,7 +2,9 @@ package com.example.android.tidyup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class RewardAndPenatly extends AppCompatActivity {
 
@@ -10,5 +12,16 @@ public class RewardAndPenatly extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reward_and_penatly);
+    }
+
+    public void OnAddReward(View view) {
+        Intent intent = new Intent(this, AddReward.class);
+        startActivity(intent);
+    }
+
+    public void OnAddPenalty(View view) {
+    }
+
+    public void OnAssignPenalty(View view) {
     }
 }
--- app/src/main/java/com/example/android/tidyup/RewardsManagement.java
+++ app/src/main/java/com/example/android/tidyup/RewardsManagement.java
@@ -1,11 +1,20 @@
 package com.example.android.tidyup;
 
 import android.os.AsyncTask;
+import android.util.Log;
 
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -13,22 +22,28 @@ public class RewardsManagement extends AsyncTask<Void, Void, Void> {
     private static final FirebaseAuth fAuth = FirebaseAuth.getInstance();
     private static final FirebaseFirestore fFirestore = FirebaseFirestore.getInstance();
     //private static final DocumentReference docRef = fFirestore.collection("Rewards").document(fAuth.getUid());
-    private static Map<String, List<String>> grDB;
+    private static Map<String, Map<String, List<String>>> grDB;
     public static int addReward(String userID, String groupID, String rewardDescription, String rewardName, int rewardVal){
         if(userID == null || groupID == null || rewardDescription == null || rewardName == null || rewardVal <= 0){
             return -1; // Missing information
         }
         return -1;
     }
-    public static String getGroupRewards(String groupID){
-        if(grDB == null)
-            return null;
-        for (Map.Entry<String, List<String>> entry : grDB.entrySet()) {
-            if (entry.getValue().contains(groupID)) {
-                return entry.getKey();
+
+    public static void readGroupRewardsDB(){
+        DocumentReference docRef = fFirestore.collection("Rewards-Penalties").document("Rewards");
+        docRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                Rewards rewardDocument = documentSnapshot.toObject(Rewards.class);
+                grDB = rewardDocument.rewardsMap;
             }
-        }
-        return null;
+        }).addOnFailureListener(new OnFailureListener() {
+            public void onFailure(@NonNull Exception e) {
+                Log.w("RewardsManagementFirebase", "Error reading document", e);
+            }
+        });
+
     }
     @Override
     protected Void doInBackground(Void... voids) {
@@ -38,5 +53,14 @@ public class RewardsManagement extends AsyncTask<Void, Void, Void> {
     @Override
     protected void onPreExecute() {
         super.onPreExecute();
+    }
+
+    private static class Rewards{
+        public Map<String, Map<String, List<String>>> rewardsMap = new HashMap<>();
+        Rewards(){}
+        Rewards(Map<String, Map<String, List<String>>> customMap){
+            this.rewardsMap = customMap;
+        }
+        public Map<String, Map<String, List<String>>> getRewardsMap() { return this.rewardsMap;}
     }
 }

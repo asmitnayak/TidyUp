4f85241f99ee42a1987ea1a8286605b1775f4d61
monica22schmidt <monica22schmidt@gmail.com>
2021-04-09 00:37:02 -0500

    Get rewardNames from map

--- app/src/main/java/com/example/android/tidyup/RewardsManagement.java
+++ app/src/main/java/com/example/android/tidyup/RewardsManagement.java
@@ -43,19 +43,17 @@ public class RewardsManagement extends AsyncTask<Void, Void, Void> {
             }else{
                 groupRewardMap.put(rewardName, Arrays.asList(new String[]{rewardDescription, String.valueOf(rewardVal), null}));
                 grDB.put(groupID, groupRewardMap);
-                RewardsManagement.Rewards rewards = new RewardsManagement.Rewards(grDB);
-                docRef.collection(COLLECTIONPATH_REWARDS_PENALTIES).document(DOCUMENTPATH_REWARDS).set(rewards);
-                return 1;
             }
 
         }else{
             Map<String, List<String>> groupRewardMap = new HashMap<>();
             groupRewardMap.put(rewardName, Arrays.asList(new String[]{rewardDescription, String.valueOf(rewardVal), null}));
             grDB.put(groupID, groupRewardMap);
-            RewardsManagement.Rewards rewards = new RewardsManagement.Rewards(grDB);
-            docRef.collection(COLLECTIONPATH_REWARDS_PENALTIES).document(DOCUMENTPATH_REWARDS).set(rewards);
-            return 1;
+
         }
+        RewardsManagement.Rewards rewards = new RewardsManagement.Rewards(grDB);
+        docRef.collection(COLLECTIONPATH_REWARDS_PENALTIES).document(DOCUMENTPATH_REWARDS).set(rewards);
+        return 1;
     }
     //Gets the reward map for the specific group
     public static Map<String, List<String>> getGroupRewardsMap(String groupID){
@@ -87,6 +85,14 @@ public class RewardsManagement extends AsyncTask<Void, Void, Void> {
         }else{
             return null;
         }
+    }
+    public static ArrayList<String> getRewardNameList(Map<String, List<String>> groupRewardMap) {
+        ArrayList<String> rewardNames = new ArrayList<String>();
+        if (groupRewardMap != null){
+            rewardNames = new ArrayList<String>(groupRewardMap.keySet());
+        }
+        return rewardNames;
+
     }
     //Initialized grDB by reading the document from the database
     public static void readGroupRewardsDB(){

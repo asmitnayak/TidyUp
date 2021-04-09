c5d83399f19e8f04ec2d3eb91007e3d944f609b3
monica22schmidt <monica22schmidt@gmail.com>
2021-04-09 15:02:14 -0500

    created popup UI

---
+++ app/src/main/java/com/example/android/tidyup/RewardAssignmentPopUp.java
@@ -0,0 +1,18 @@
+package com.example.android.tidyup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class RewardAssignmentPopUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reward_assignment_pop_up);
+    }
+
+    public void onClaim(View view) {
+    }
+}

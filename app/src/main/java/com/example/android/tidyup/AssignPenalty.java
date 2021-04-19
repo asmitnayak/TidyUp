25828757a88f734f0ae09131fa47447764fc6c76
monica22schmidt <monica22schmidt@gmail.com>
2021-04-19 02:11:46 -0500

    Implemented PenaltyManagement similar to RewardManagement. Did not implement updater

--- app/src/main/java/com/example/android/tidyup/AssignPenalty.java
+++ app/src/main/java/com/example/android/tidyup/AssignPenalty.java
@@ -3,6 +3,7 @@ package com.example.android.tidyup;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class AssignPenalty extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class AssignPenalty extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_assign_penalty);
+    }
+
+    public void onAssignPenalty(View view) {
     }
 }
